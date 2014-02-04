aeee2e15e0ab19ed4a77ecbd4de5a75041321097
Todd Wolfson <todd@twolfson.com>
2014-02-04 00:19:27 -0800

    Fixed up child processes

--- test/foundry-release-git_test.js
+++ test/foundry-release-git_test.js
@@ -1,9 +1,9 @@
 // Load in dependencies
+var exec = require('child_process').exec;
 var path = require('path');
 var expect = require('chai').expect;
 var wrench = require('wrench');
 var gitRelease = require('../');
-var childUtils = require('./utils/child-process');
 var fixtureUtils = require('./utils/fixtures');
 
 // TODO: Move git init logic into its own utility
@@ -23,13 +23,13 @@ describe('Publishing', function () {
     // TODO: Use premade git directory a la sexy-bash-prompt
     before(function initializeGitFolder (done) {
       process.chdir(this.gitDir);
-      childUtils.iKnowWhatIAmDoingExec('git init', function (err, stdout, stderr) {
+      exec('git init', function (err, stdout, stderr) {
         if (err) { return done(err); }
-        childUtils.iKnowWhatIAmDoingExec('touch a', function (err, stdout, stderr) {
+        exec('touch a', function (err, stdout, stderr) {
           if (err) { return done(err); }
-          childUtils.iKnowWhatIAmDoingExec('git add -A', function (err, stdout, stderr) {
+          exec('git add -A', function (err, stdout, stderr) {
             if (err) { return done(err); }
-            childUtils.iKnowWhatIAmDoingExec('git commit -m "Initial commit =D"', function (err, stdout, stderr) {
+            exec('git commit -m "Initial commit =D"', function (err, stdout, stderr) {
               done(err);
             });
           });
@@ -46,7 +46,7 @@ describe('Publishing', function () {
     });
 
     it('adds a git tag', function (done) {
-      childUtils.iKnowWhatIAmDoingExec('git tag', function (err, stdout, stderr) {
+      exec('git tag', function (err, stdout, stderr) {
         if (err) {
           return done(err);
         }
@@ -56,7 +56,7 @@ describe('Publishing', function () {
     });
 
     it('adds a git commit', function (done) {
-      childUtils.iKnowWhatIAmDoingExec('git log --format=oneline -n 1', function (err, stdout, stderr) {
+      exec('git log --format=oneline -n 1', function (err, stdout, stderr) {
         if (err) {
           return done(err);
         }
