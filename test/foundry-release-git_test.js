c91bf125acef27c41f1f59ec9da27395d3c33bd5
Todd Wolfson <todd@twolfson.com>
2014-03-10 02:35:16 -0700

    Reintroducing publish tests

--- test/foundry-release-git_test.js
+++ test/foundry-release-git_test.js
@@ -49,3 +49,48 @@ describe('Committing', function () {
     });
   });
 });
+
+describe.only('Publishing', function () {
+  describe('in a git folder', function () {
+    fixtureUtils.bundle.mkdir('publish_test');
+    fixtureUtils.bundle.exec('git init');
+    fixtureUtils.bundle.exec('touch a');
+    fixtureUtils.bundle.exec('git add -A');
+    fixtureUtils.bundle.exec('git commit -m "Initial commit =D"');
+    var _exec = childProcess.exec;
+    before(function stubExec () {
+      var that = this;
+      this.execCalls = [];
+      childProcess.exec = function (/* cmd, options, cb */) {
+        // Preserve the arguments
+        var args = [].slice.call(arguments);
+        that.execCalls.push(args);
+
+        // Run the callback (err, stdout, stderr)
+        var cb = args[args.length - 1];
+        console.log(args);
+        cb(null, '', '');
+      };
+    });
+    after(function restoreExec () {
+      childProcess.exec = _exec;
+    });
+
+    before(function publish (done) {
+      this.inBundle(function () {
+        gitRelease.publish({
+          version: '0.1.0',
+          message: 'Release 0.1.0',
+          description: null
+        }, done);
+      });
+    });
+
+    it.skip('publishes the commit', function () {
+
+    });
+    it.skip('publishes the tags', function () {
+
+    });
+  });
+});
